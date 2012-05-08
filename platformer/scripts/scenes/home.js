b174558b425b857a3fbe5544a9b6c26e79085d92
Adrian Smith <adrian@Adrian-Smiths-MacBook-Pro.local>
2012-05-08 10:10:21 -0700

    added home page text.

--- platformer/scripts/scenes/home.js
+++ platformer/scripts/scenes/home.js
@@ -1,6 +1,13 @@
 re.scene('home')
 .enter(function(){
-  
+    re.e('text')
+        .attr({
+            text:'Gameover! Press space to play again.',
+            textColor:'#000000',
+            posX:300,
+            posY:300,
+            font:'60px'
+        });
     this.keyboard = re.e('keyboard')
         .on('keydown:space', function(key, event){
             re.scene('play').enter('level01');
